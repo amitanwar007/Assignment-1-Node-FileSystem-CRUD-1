3963c3b58be445cc7ac09ec1322a134c46c402a5
Amit Kumar <amitanwar007@gmail.com>
2022-12-12 15:16:42 +0530

    i have added data to git

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/App.css";
+import Fruits from "./Fruits";
+
+const App = () => {
+  return (
+    <div id="main">
+      <h1>Types of Food:</h1>
+      {/* Change code below this line */}
+      <Fruits />
+      {/* Change code above this line */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Fruits.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TypesOfFruit from "./TypesOfFruit";
+const Fruits = () => {
+  return (
+    <div id="fruits">
+      {/* Change code below this line */}
+      <TypesOfFruit />
+      {/* Change code above this line */}
+    </div>
+  );
+};
+export default Fruits;
